113f3d20fb5e4433e368ff4e47c98589944d1973
LWSdevelopers <lwspinn@gmail.com>
2025-06-19 19:40:23 +0200

    Add files via upload

---
+++ 0001anuncio.js
@@ -0,0 +1,10 @@
+const espaco = document.getElementById('espaco-anuncio');
+
+espaco.innerHTML = `
+  <div class="anuncio-conteudo">
+  <img src="https://mozentretenimento.co.mz/wp-content/uploads/2023/11/Tudo-sobre-Lil-Wayne-de-Moz.jpg" alt="Patrocinado" style="max-width: 100%; height: 100%; border-radius: 4px; margin: 01px 0;">
+    <h><strong>PATROCINADO</strong> • Compre já, tens uma oferta de 30% de desconto.</h>
+    
+  </div><p></p>
+  <a href="https://forms.gle/BNm9EiTt74HP9WLc8?target=blank" target="_blank" class="botao-cta">COMPRAR AGORA</a>
+`;
