50cf558d699303d9d693c22a00f5c835ec63f3cd
LWSdevelopers <lwspinn@gmail.com>
2025-06-30 16:26:24 +0200

    Add files via upload

---
+++ aaanuncio.js
@@ -0,0 +1,9 @@
+const espaco = document.getElementById('espaco-anuncio');
+
+espaco.innerHTML = `
+  <div class="anuncio-conteudo">
+    <h><strong>PATROCINADO</strong> • Esse produto vai mudar sua vida! Agora com 50% de desconto por tempo limitado</h>
+    
+  </div><p></p>
+  <a href="https//:google.com" class="botao-cta"  target="_blank" >COMPRAR AGORA</a>
+`;
