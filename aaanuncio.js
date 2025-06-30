6ccab71cbff41e4efe1dcca0bbf62c63770e1e86
LWSdevelopers <lwspinn@gmail.com>
2025-06-30 17:28:39 +0200

    Add files via upload

--- aaanuncio.js
+++ aaanuncio.js
@@ -2,8 +2,8 @@ const espaco = document.getElementById('espaco-anuncio');
 
 espaco.innerHTML = `
   <div class="anuncio-conteudo">
-    <h><strong>PATROCINADO</strong> • Esse produto vai mudar sua vida! Agora com 50% de desconto por tempo limitado</h>
+    <h><strong>PATROCINADO</strong> • Para multas e ferimentos graves, use o cinto de segurança.</h>
     
   </div><p></p>
-  <a href="https//:google.com" class="botao-cta"  target="_blank" >COMPRAR AGORA</a>
+  <a href="https//:google.com" class="botao-cta"  target="_blank" >LER MAIS</a>
 `;
