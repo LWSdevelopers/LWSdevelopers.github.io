d5b5cfd5ba9eeeb07ec0ecfb5922aca26e131139
LWSdevelopers <lwspinn@gmail.com>
2025-06-30 20:09:46 +0200

    Add files via upload

--- aaanuncio.js
+++ aaanuncio.js
@@ -5,5 +5,5 @@ espaco.innerHTML = `
     <h><strong>PATROCINADO</strong> • Para multas e ferimentos graves, use o cinto de segurança.</h>
     
   </div><p></p>
-  <a href="https//:google.com" class="botao-cta"  target="_blank" >LER MAIS</a>
+  <a href="https://www.google.com/search?q=exames+de+condu%C3%A7%C3%A3o+online&oq=&gs_lcrp=EgZjaHJvbWUqCQgAECMYJxjqAjIJCAAQIxgnGOoCMgkIARAjGCcY6gIyCQgCECMYJxjqAjIJCAMQIxgnGOoCMgkIBBAjGCcY6gIyCQgFECMYJxjqAjIJCAYQIxgnGOoCMgkIBxAjGCcY6gIyCQgIECMYJxjqAjIJCAkQIxgnGOoCMgkIChAjGCcY6gIyCQgLECMYJxjqAjIJCAwQIxgnGOoCMgkIDRAjGCcY6gIyCQgOECMYJxjqAjIRCA8QABgDGEIYjwEYtAIY6gIyEQgQEAAYAxhCGI8BGLQCGOoCMg8IERAuGAMYjwEYtAIY6gIyEQgSEAAYAxhCGI8BGLQCGOoCMhEIExAAGAMYQhiPARi0AhjqAtIBBi0xajBqN6gCFLACAQ&client=ms-android-oppo&sourceid=chrome-mobile&ie=UTF-8" class="botao-cta" target="_blank">LER MAIS</a>
 `;
