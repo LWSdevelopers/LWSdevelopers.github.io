360d285b484d1c38ea8beaccbd3189d94652026d
LWSdevelopers <lwspinn@gmail.com>
2025-10-19 12:46:18 +0200

    Add files via upload

---
+++ 000ads.js
@@ -0,0 +1,21 @@
+const anuncioData = {
+  imagem: "",
+  titulo: "⭐Gostou do aplicativo?",
+  descricao: "Avalie o app na Play Store e conte sua experiência, leva menos de 10 segundos! Sua opinião ajuda muito.",
+  link: "https://abre.ai/teoricomz?target=blank",
+  textoBotao: "Avaliar agora"
+};
+
+const anuncioContainer = document.getElementById("anuncio");
+
+anuncioContainer.innerHTML = `
+  <div class="patrocinado"></div>
+  <img src="${anuncioData.imagem}" alt="" class=""/>
+  <div class="conteudo-anuncio">
+    <h3>${anuncioData.titulo}</h3>
+    <p>${anuncioData.descricao}</p>
+  </div>
+  <button class="botao-anuncio" target="_blank" onclick="window.open('${anuncioData.link}', '_blank')">
+    ${anuncioData.textoBotao}
+  </button>
+`;
