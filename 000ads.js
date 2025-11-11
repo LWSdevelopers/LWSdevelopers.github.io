363d70e838932b870424e510531fb4a0f7c7ae7e
LWSdevelopers <lwspinn@gmail.com>
2025-11-11 21:52:46 +0200

    Add files via upload

--- 000ads.js
+++ 000ads.js
@@ -1,16 +1,16 @@
 const anuncioData = {
-  imagem: "",
-  titulo: "⭐Gostou do aplicativo?",
-  descricao: "Avalie o app na Play Store e conte sua experiência, leva menos de 10 segundos! Sua opinião ajuda muito.",
-  link: "https://abre.ai/teoricomz?target=blank",
-  textoBotao: "Avaliar agora"
+  imagem: "https://lwsdevelopers.github.io/000abanner.png",
+  titulo: "Promoção de Megas! Aproveita!",
+  descricao: "Megas rápido e barato! Temos pacotes para todas as necessidades.",
+  link: "https://abreai.com/Magonet?target=blank",
+  textoBotao: "Contactar"
 };
 
 const anuncioContainer = document.getElementById("anuncio");
 
 anuncioContainer.innerHTML = `
-  <div class="patrocinado"></div>
-  <img src="${anuncioData.imagem}" alt="" class=""/>
+  <div class="patrocinado">Patrocinado</div>
+  <img src="${anuncioData.imagem}" alt="Imagem do curso" class="imagem-anuncio"/>
   <div class="conteudo-anuncio">
     <h3>${anuncioData.titulo}</h3>
     <p>${anuncioData.descricao}</p>
