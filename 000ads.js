40248aaffe8feba8303efffdb71b7401d391cca9
LWSdevelopers <lwspinn@gmail.com>
2026-01-25 10:43:51 +0200

    Update 000ads.js

--- 000ads.js
+++ 000ads.js
@@ -1,7 +1,7 @@
 const anuncioData = {
   imagem: "https://lwsdevelopers.github.io/000abanner.png",
   titulo: "🤩 Gostou do aplicativo?",
-  descricao: "Avalie o app no Play Store e conte a sua experiência, leva menos de 10 segundos!",
+  descricao: "Avalie o app no Play Store e conte a sua experiência, isso leva menos de 10 segundos!",
   link: "https://abre.ai/feedbackapp?target=blank",
   textoBotao: "Avaliar"
 };
@@ -9,7 +9,7 @@ const anuncioData = {
 const anuncioContainer = document.getElementById("anuncio");
 
 anuncioContainer.innerHTML = `
-  <div class="patrocinado">Patrocinado • Anúncio</div>
+  <div class="patrocinado">•</div>
   <div class="conteudo-anuncio">
     <h4>${anuncioData.titulo}</h4>
     <p>${anuncioData.descricao}</p>
