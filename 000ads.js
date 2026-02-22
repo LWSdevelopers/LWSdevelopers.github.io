afaf91dd55d7026594c739807ec2ac41d64a5092
LWSdevelopers <lwspinn@gmail.com>
2026-02-22 19:40:32 +0200

    Update 000ads.js

--- 000ads.js
+++ 000ads.js
@@ -1,9 +1,9 @@
 const anuncioData = {
   imagem: "https://lwsdevelopers.github.io/000abanner.png",
-  titulo: "🤩 Megabytes-MB quase de borla.",
-  descricao: "Mais internet para navegar, estudar, conversar e ver vídeos. Aproveita já!",
-  link: "https://abre.ai/mb-freeee?target=blank",
-  textoBotao: "Ativar agora"
+  titulo: "🤩 Gostou do aplicativo?",
+  descricao: "Avalie o app no play store, leva menos de 10 segundos e a conte sua experiência! ",
+  link: "https://abre.ai/teoricomz-app?target=blank",
+  textoBotao: "Avaliar"
 };
 
 const anuncioContainer = document.getElementById("anuncio");
