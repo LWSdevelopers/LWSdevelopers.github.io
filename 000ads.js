346a505aacde97a2846757b4db3f241909ebdda0
LWSdevelopers <lwspinn@gmail.com>
2026-06-01 07:59:39 +0200

    Add files via upload

--- 000ads.js
+++ 000ads.js
@@ -1,18 +1,16 @@
 const anuncioData = {
-  imagem: "https://lwsdevelopers.github.io/000img.jpg",
-  titulo: "",
-  descricao: "🚦 O método profissional para cuidar, poupar, criar soluções e evitar grandes prejuízos.",
-  link: "https://abre.ai/picl?target=blank",
-  textoBotao: "Descobrir Agora"
+  titulo: "😍 Internet (MB) Quase de Borla!",
+  descricao: "Rápido e Seguro: Assista vídeos, estude e navegue com dados baratos.",
+  link: "https://abre.ai/mb-adquirir?target=blank",
+  textoBotao: "Pedir Megas"
 };
 
 const anuncioContainer = document.getElementById("anuncio");
 
 anuncioContainer.innerHTML = `
-  <div class="patrocinado">Anúncio •</div>
-  <img src="${anuncioData.imagem}" alt="Imagem do curso" class="imagem-anuncio"/>
+  <div class="patrocinado">Anúncio • </div>
   <div class="conteudo-anuncio">
-   <h3>${anuncioData.titulo}</h3>
+    <h3>${anuncioData.titulo}</h3>
     <p>${anuncioData.descricao}</p>
   </div>
   <button class="botao-anuncio" target="_blank" onclick="window.open('${anuncioData.link}', '_blank')">
