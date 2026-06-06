b5f4533c52ec55572653a9e2b4afefa8ec5b3781
LWSdevelopers <lwspinn@gmail.com>
2026-06-06 20:53:43 +0200

    Add files via upload

--- 000ads.js
+++ 000ads.js
@@ -1,14 +1,14 @@
 const anuncioData = {
-  titulo: "😍 Internet (MB) Quase de Borla!",
-  descricao: "Rápido e Seguro: Assista vídeos, estude e navegue com dados baratos.",
-  link: "https://abre.ai/mb-adquirir?target=blank",
-  textoBotao: "Pedir Megas"
+  titulo: "😍 Gostou do aplicativo?",
+  descricao: "Avalie o app na play store e conte a sua experiência, leva menos de 10 segundos",
+  link: "https://abre.ai/rateapp?target=blank",
+  textoBotao: "Avaliar"
 };
 
 const anuncioContainer = document.getElementById("anuncio");
 
 anuncioContainer.innerHTML = `
-  <div class="patrocinado">Anúncio • </div>
+  <div class="patrocinado"> • </div>
   <div class="conteudo-anuncio">
     <h3>${anuncioData.titulo}</h3>
     <p>${anuncioData.descricao}</p>
