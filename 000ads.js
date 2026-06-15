26e937788e1e870b6b1e1f4c728c648292577e4e
LWSdevelopers <lwspinn@gmail.com>
2026-06-15 08:41:55 +0200

    Add files via upload

--- 000ads.js
+++ 000ads.js
@@ -1,16 +1,18 @@
 const anuncioData = {
-  titulo: "😍 Gostou do aplicativo?",
-  descricao: "Avalie o app na play store e conte a sua experiência, leva menos de 10 segundos",
-  link: "https://abre.ai/rateapp?target=blank",
-  textoBotao: "Avaliar"
+  imagem: "000img.jpg",
+  titulo: "",
+  descricao: "🚦 O método profissional para cuidar, poupar, criar soluções e evitar grandes prejuízos.",
+  link: "https://abre.ai/picl?target=blank",
+  textoBotao: "Descobrir Agora"
 };
 
 const anuncioContainer = document.getElementById("anuncio");
 
 anuncioContainer.innerHTML = `
-  <div class="patrocinado"> • </div>
+  <div class="patrocinado">Anúncio •</div>
+  <img src="${anuncioData.imagem}" alt="Imagem do curso" class="imagem-anuncio"/>
   <div class="conteudo-anuncio">
-    <h3>${anuncioData.titulo}</h3>
+   <h3>${anuncioData.titulo}</h3>
     <p>${anuncioData.descricao}</p>
   </div>
   <button class="botao-anuncio" target="_blank" onclick="window.open('${anuncioData.link}', '_blank')">
