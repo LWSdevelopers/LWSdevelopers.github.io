46aa94012e411e4fa4e2537f4a9c83af7342719b
LWSdevelopers <lwspinn@gmail.com>
2026-09-08 11:42:12 +0200

    Add files via upload

--- 000ads.js
+++ 000ads.js
@@ -1,16 +1,16 @@
 const anuncioData = {
   imagem: "https://raw.githubusercontent.com/LWSdevelopers/LWSdevelopers.github.io/refs/heads/main/000img.jpg",
-  titulo: "",
-  descricao: "🚦 Descubra os erros que podem fazer você gastar milhares de meticais no carro.",
-  link: "https://abre.ai/picl?target=blank",
-  textoBotao: "Ver Oferta"
+  titulo: "🚘 A SUA MARCA AQUI",
+  descricao: "Chegue a milhares de futuros condutores em Moçambique.",
+  link: "https://wa.me/258855495032?text=Ol%C3%A1%21%20%F0%9F%91%8B%20Tenho%20interesse%20em%20anunciar%20no%20vosso%20aplicativo.%20Gostaria%20de%20saber%20mais%20informa%C3%A7%C3%B5es.%20%5BEdite%20esta%20mensagem%20e%20conte-nos%20como%20podemos%20ajudar.%5D?target=blank",
+  textoBotao: "Contacte-nos"
 };
 
 const anuncioContainer = document.getElementById("anuncio");
 
 anuncioContainer.innerHTML = `
-  <div class="patrocinado">Anúncio •</div>
-  <img src="${anuncioData.imagem}" alt="Imagem do curso" class="imagem-anuncio"/>
+  <div class="patrocinado">Espaço publicitário •</div>
+  <img src="${anuncioData.imagem}" alt="Publicidade" class="imagem-anuncio"/>
   <div class="conteudo-anuncio">
    <h3>${anuncioData.titulo}</h3>
     <p>${anuncioData.descricao}</p>
